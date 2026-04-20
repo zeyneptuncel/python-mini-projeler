05a613c5aa3bc0f79662198ea3b945427db58d84
Zeynep Tuncel <zeynepqtuncel@gmail.com>
2026-04-20 22:48:55 +0300

    Oyundaki çıktı metni düzeltildi

--- carpmaOyunu.py
+++ carpmaOyunu.py
@@ -12,10 +12,10 @@ while True:
     print(f"{a} x {b} = ?")
     tahmin= int(input("Cevabınız: "))
     if tahmin == cevap:  
-        print("\033[92mAFERİN KIZZZ!\033[0m")
+        print("\033[92mTebrikler!\033[0m")
         puan += 10
     else:
-        print("\033[91mBİLEMEDİNN CANIMM!\033[0m")
+        print("\033[91mYanlış Cevap!\033[0m")
 
  print(f"--------------\nToplam puan: {puan}")
  tekrar = input("\nTekrar oynamak için 'R' tuşuna, çıkmak için herhangi bir tuşa bas: ")
